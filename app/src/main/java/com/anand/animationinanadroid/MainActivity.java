aa83a1237d644fb2ef6ca2906809c69a4c2cf131
Anand Raj <anandraja1993.15@gmail.com>
2019-01-28 17:36:27 +0530

    mainActivity log commit

--- app/src/main/java/com/anand/animationinanadroid/MainActivity.java
+++ app/src/main/java/com/anand/animationinanadroid/MainActivity.java
@@ -1,13 +1,27 @@
 package com.anand.animationinanadroid;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHelloWorld = findViewById(R.id.txtHelloWorld);
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Log.i("MYTAG","HELLO WORLD!");
+            }
+        });
     }
 }
